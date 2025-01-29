42cb863a0fce8e8ecbcadb127c72f06d31d59dc8
Mahima <maahiisingh3@gmail.com>
2025-01-29 23:18:27 +0530

    today's code

---
+++ narrowing/NarrowTypeCast.java
@@ -0,0 +1,40 @@
+package narrowing;
+
+public class NarrowTypeCast {
+	 public static void main(String[] args) {
+
+		//----------double to integer-----------------
+		 double d = 10.5;
+	     System.out.println(d);
+	    
+	    
+	    int i = (int)d;
+	    System.out.println(i);
+	    
+	    //-----------------int to short------------------
+	    
+	    int a = 7;
+	    System.out.println(a);
+	    
+	    short s = (short)a;
+	    System.out.println(a);
+	    
+	    //----------char to int----------------
+	    
+	    char c = 'm';
+	    System.out.println(c);
+	    
+	    int e = (int)c;
+	    System.out.println(e);
+	    
+	    //double to float----------------------
+	    float g =(float)d;
+	    System.out.println(g);
+	    
+	    
+	    float f = (float)c;
+	    
+	    
+	    
+	}
+}
